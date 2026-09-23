bc14d5a418f3a7d33eb27db7a72d1bf4b774a2ff
Nerlylli <katenerlycadet@gmail.com>
2026-09-23 13:46:00 -0400

    Added Lilin's parts

--- EXERCISES/E1/Ex_1.py
+++ EXERCISES/E1/Ex_1.py
@@ -1,3 +1,25 @@
+# Task 1: Add parentheses to the Python statement below so that it prints
+# out the number 7.
+
+print((10 + 4) / 2)
+
+#------------------------------------------------------------------------
+
+# Task 2: Change the operator in the statement below so that it displays
+# "True" instead of "False."
+
+print(14 < 15)
+
+#------------------------------------------------------------------------
+
+# Task 3: Change the variable assignment below so that the print statement
+# displays "54." (Don't change the print statement!)
+
+a_num_variable = 54
+print(a_num_variable)
+
+#------------------------------------------------------------------------
+
 # Task 4: Three variables are assigned below, all with different types.
 # Replace the word "None" inside the parentheses of type() in the print
 # statement below so that it prints "".
@@ -46,3 +68,57 @@ print(x)
 
 y = partLy.strip().upper()
 print(y) 
+
+#------------------------------------------------------------------------
+
+# Task 9: Modify the value assigned to variable "offset" below so that
+# the following "print" statement displays the letter "p".
+
+offset = 1
+print("apple"[offset])
+
+#------------------------------------------------------------------------
+
+# Task 10: Modify the values assigned to variables "start" and "end"
+# below so that the following "print" statement displays the word "jump".
+
+start = 12
+end = 16
+aStringSentenceAgain = "Did the cat jump out the window yesterday?"
+print(aStringSentenceAgain[start:end])
+
+#------------------------------------------------------------------------
+
+# Task 11: Modify the statement below so that it displays the number 100.
+# Do this using the int() function (hint: you need to use it twice).
+
+print(int("19") + int("81"))
+#------------------------------------------------------------------------
+
+# Task 12: Modify the code below to include a condition to print the statement  
+# 'test_var is less than 200' for the current value of test_var. 
+# Do not change the intial code, rather add to it
+test_var = 90
+if test_var > 200:	
+	print("test_var is greater than 200!")
+else:
+	print("test_var is less than 200")
+#------------------------------------------------------------------------
+
+# Task 13: Modify the if statement below to print the statement
+# 'the condition test passed'. Do not change the values of the varaibles.
+test_var_three = 400
+test_var_two = 800
+if test_var_three > 200 and test_var_two > 400:	
+	print("the condition test passed")
+else:
+	print("the condition test not passed")
+#------------------------------------------------------------------------
+
+# Task 14: A variable "greek" is defined below. The value of this variable
+# is of type list. Change the expression below the variable definition so
+# that it prints "alpha" (instead of "beta").
+
+greek = ["alpha", "beta", "gamma", "delta", "epsilon"]
+print(greek[0])
+
